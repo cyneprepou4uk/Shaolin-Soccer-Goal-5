ac07e49eb0e86f70a04aff809f954d00ac2fd113
cyneprepou4uk <cyneprepou4uk@gmail.com>
2020-05-03 13:47:18 +0300

    000 init
    использовать как базовый исходник

--- ca65.bat
+++ ca65.bat
@@ -1,4 +1,6 @@
 start /wait translate.pl
+:: для запуска скрипта требуется Strawberry Perl
+:: http://strawberryperl.com
 
 ca65 bank_00_copy.asm
 ca65 bank_01_copy.asm
@@ -11,7 +13,7 @@ ca65 bank_07_copy.asm
 
 ld65 -C ld65.cfg bank_00_copy.o bank_01_copy.o bank_02_copy.o bank_03_copy.o bank_04_copy.o bank_05_copy.o bank_06_copy.o bank_07_copy.o
 
-copy /B header.bin + bank_00_copy.bin + bank_01_copy.bin + bank_02_copy.bin + bank_03_copy.bin + bank_04_copy.bin + bank_05_copy.bin + bank_06_copy.bin + bank_07_copy.bin + chr.chr test.nes
+copy /B header.bin + bank_00_copy.bin + bank_01_copy.bin + bank_02_copy.bin + bank_03_copy.bin + bank_04_copy.bin + bank_05_copy.bin + bank_06_copy.bin + bank_07_copy.bin + chr.chr goal5.nes
 
 del *.o
 del bank_*.bin
