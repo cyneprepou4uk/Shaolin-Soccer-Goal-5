b1d940232cf442b2fcbd63683f5e82704a060b1c
cyneprepou4uk <cyneprepou4uk@gmail.com>
2020-05-09 16:46:14 +0300

    удален лишний код

--- ca65.bat
+++ ca65.bat
@@ -2,21 +2,23 @@ start /wait translate.pl
 :: для запуска скрипта требуется Strawberry Perl
 :: http://strawberryperl.com
 
-ca65 bank_00_copy.asm
-ca65 bank_01_copy.asm
-ca65 bank_02_copy.asm
-ca65 bank_03_copy.asm
-ca65 bank_04_copy.asm
-ca65 bank_05_copy.asm
-ca65 bank_06_copy.asm
-ca65 bank_07_copy.asm
+ca65 -l bank_00_copy.asm
+ca65 -l bank_01_copy.asm
+ca65 -l bank_02_copy.asm
+ca65 -l bank_03_copy.asm
+ca65 -l bank_04_copy.asm
+ca65 -l bank_05_copy.asm
+ca65 -l bank_06_copy.asm
+ca65 -l bank_07_copy.asm
+
+echo off
 
 ld65 -C ld65.cfg bank_00_copy.o bank_01_copy.o bank_02_copy.o bank_03_copy.o bank_04_copy.o bank_05_copy.o bank_06_copy.o bank_07_copy.o
 
 copy /B header.bin + bank_00_copy.bin + bank_01_copy.bin + bank_02_copy.bin + bank_03_copy.bin + bank_04_copy.bin + bank_05_copy.bin + bank_06_copy.bin + bank_07_copy.bin + chr.chr goal5.nes
 
-del *.o
-del bank_*.bin
-del *_copy.*
+del *.o + bank_*.bin + *_copy.asm + *_copy.inc + _bank_*_copy.lst
 
-pause
+ren bank_*_copy.lst _bank_*_copy.lst
+
+timeout 5
